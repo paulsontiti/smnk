9a8d999dc47db637fcc38db4f0fee11477443f5c
paulsontiti <paulsontiti@gmail.com>
2023-03-12 09:02:19 +0100

    Update whySmnk.tsx

--- src/components/home/whySmnk.tsx
+++ src/components/home/whySmnk.tsx
@@ -12,30 +12,7 @@ export default function WhySMNK(){
             margin:"2rem 5rem"
 
         }}>
-            <Card>
-                <CardHeader title="Why SMNK?"></CardHeader>
-                <CardContent> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-                     a type specimen book. 
-                        <Link href='/whysmnk' style={{
-                                    textDecoration:"underline", color:"blue"
-                                }}>
-                            Read more
-                        </Link>
-                     </CardContent>
-            </Card>
-            <Card>
-                <CardHeader title="What Problems Do SMNK Solve?"></CardHeader>
-                <CardContent> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-                     a type specimen book. <Link href='/whysmnk' style={{textDecoration:"underline", color:"blue"}}>Read more</Link></CardContent>
-            </Card>
-            <Card>
-                <CardHeader title="How To Use SMNK"></CardHeader>
-                <CardContent> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
-                     a type specimen book. <Link href='/whysmnk' style={{textDecoration:"underline", color:"blue"}}>Read more</Link></CardContent>
-            </Card>
+           
         </Paper>
     )
 }
