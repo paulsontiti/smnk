dbc90f37b3847239e4d9c68fc4663cf6ff8e37a2
paulsontiti <paulsontiti@gmail.com>
2023-03-12 09:13:18 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,6 @@
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 
 import HomeMenuDrawer from './home/mobile/menuDrawer';
-import { useMediaQuery } from '@material-ui/core';
 import { Theme, useTheme } from '@mui/material/styles';
 import MobileHeader from './home/mobile/mobileHeader';
 import DesktopHeader from './home/desktop/desktopHeader';
@@ -12,16 +9,14 @@ import DesktopHeader from './home/desktop/desktopHeader';
 
 export default function Header() {
 
-  //mdedia query
-  const theme : Theme  = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+ 
 
  
 
   return (
     <div >
        
-          {matches ? <MobileHeader/> :  <DesktopHeader/>}
+          <DesktopHeader/>
            
           
       
