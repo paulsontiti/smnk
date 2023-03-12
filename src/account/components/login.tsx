d37e7adead35d9b55c11b8c076346d1c4e08f5fe
paulsontiti <paulsontiti@gmail.com>
2023-03-12 10:09:03 +0100

    Update login.tsx

--- src/account/components/login.tsx
+++ src/account/components/login.tsx
@@ -24,7 +24,7 @@ const submitHandler = async (values:{email:string,password:string})=>{
       try{
         const res = await axios({
           method:'POST',
-          url:'http://localhost:3000/api/users/login',
+          url:'https://smnk-v1-0.vercel.app/api/users/login',
           data:values
       })
       const data = await res.data
