6ff23364afbddd030d2da796e1c0596f7aac4316
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-05-20 22:06:15 +0100

    job

--- src/lib/multer/index.ts
+++ src/lib/multer/index.ts
@@ -18,7 +18,7 @@ export const multerHandler = nextConnect({
 export const multerUpload= (storageUrl:string)=>{
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(process.cwd(),'/tmp/public',storageUrl))
+        cb(null, path.join(process.cwd(),'public/tmp/',storageUrl))
       },
       filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
