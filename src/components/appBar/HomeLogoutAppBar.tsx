3d490fee6f607b3b88252e4402c402c4d0801b93
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-06-06 10:41:01 +0100

    smnk

--- src/components/appBar/HomeLogoutAppBar.tsx
+++ src/components/appBar/HomeLogoutAppBar.tsx
@@ -48,7 +48,7 @@ export default function HomeLogoutAppBar() {
                 SMNK
               </Typography>
             </Grid>
-            <Grid item xs={3} display={'flex'} alignItems={'center'} justifyContent={'flex-end'}>
+            <Grid item xs={3} display={'flex'} alignItems={'center'} justifyContent={'flex-end'} ml={3}>
               <SearchDrawer />
             </Grid>
             <Grid item xs={4} display={'flex'} alignItems={'center'} justifyContent={'center'}>
