ee7d243f99ecdeac02b1807287ef9bf5dbf0860a
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-06-07 15:26:59 +0100

    smnk

--- src/pages/api/chat/see-chats.ts
+++ src/pages/api/chat/see-chats.ts
@@ -7,11 +7,15 @@ export default async function handler(req:any,res:any){
     //get database connection
     await dbConnect()
     const {receiverId} = req.body
-        try{
-           const chats = await Chat.updateMany({receiverId},{seen:true})
-                res.status(201).json('')
-        }catch(err){
-            console.log(err)
-            res.status(400).json({message:"Sorry an error occurred,please try again"})
+        if(receiverId){
+            try{
+                const chats = await Chat.updateMany({receiverId},{seen:true})
+                     res.status(201).json('')
+             }catch(err){
+                 console.log(err)
+                 res.status(400).json({message:"Sorry an error occurred,please try again"})
+             }
+        }else{
+            res.status(400).json({message:"Invalid request"})
         }
 }
--- src/pages/api/chat/unseen-chats.ts
+++ src/pages/api/chat/unseen-chats.ts
@@ -7,11 +7,15 @@ export default async function handler(req:any,res:any){
     //get database connection
     await dbConnect()
     const {receiverId} = req.body
+       if(receiverId){
         try{
-          const chats =  await Chat.find({receiverId,seen:false})
-                res.status(201).json(chats.length)
-        }catch(err){
-            console.log(err)
-            res.status(400).json({message:"Sorry an error occurred,please try again"})
-        }
+            const chats =  await Chat.find({receiverId,seen:false})
+                  res.status(201).json(chats.length)
+          }catch(err){
+              console.log(err)
+              res.status(400).json({message:"Sorry an error occurred,please try again"})
+          }
+       }else{
+        res.status(400).json({message:"Invalid request"})
+       }
 }
