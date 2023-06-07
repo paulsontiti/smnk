5c607d546070d45afa10bb17b8c80a6669d6ac2c
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-06-07 14:49:59 +0100

    smnk

--- src/account/components/signup.tsx
+++ src/account/components/signup.tsx
@@ -56,7 +56,7 @@ export default function SignUp() {
           break;
       }
     }
-  }, [user, router]);
+  }, [user, router,successful,response]);
 
   //sign up submit handler
   const submitHandler = async (values: signUpDetails) => {
