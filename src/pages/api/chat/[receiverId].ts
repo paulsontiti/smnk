b228776263742bb772d1c3458fb3f44afa6dfe8c
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-06-07 14:16:33 +0100

    smnk

--- src/pages/api/chat/[receiverId].ts
+++ src/pages/api/chat/[receiverId].ts
@@ -8,38 +8,44 @@ export default async function handler(req: any, res: any) {
   const {
     query: { receiverId },
   } = req;
-  try {
-    const receivedChats = await Chat.aggregate([
-      { $match: { receiverId: new ObjectId(receiverId) } },
-      { $group: { _id: "$senderId" } },
-      { $project: { senderId: "$_id", _id: false } },
-    ]);
-    const sentChats = await Chat.aggregate([
-      { $match: { senderId: new ObjectId(receiverId) } },
-      { $group: { _id: "$receiverId" } },
-      { $project: { receiverId: "$_id", _id: false } },
-    ]);
-
-    let filterredReceivedChats: any[] = [];
-    if(sentChats.length > 0){
-
-      sentChats.map((sentChat: any, i) => {
-        filterredReceivedChats = receivedChats.map((receivedChat: any) => {
-          if (
-            receivedChat.senderId.toString() === sentChat.receiverId.toString()
-          ) {
-            sentChats[i] = undefined;
-          }
-          return receivedChat;
+  if(receiverId){
+    try {
+      const receivedChats = await Chat.aggregate([
+        { $match: { receiverId: new ObjectId(receiverId) } },
+        { $group: { _id: "$senderId" } },
+        { $project: { senderId: "$_id", _id: false } },
+      ]);
+      const sentChats = await Chat.aggregate([
+        { $match: { senderId: new ObjectId(receiverId) } },
+        { $group: { _id: "$receiverId" } },
+        { $project: { receiverId: "$_id", _id: false } },
+      ]);
+  
+      let filterredReceivedChats: any[] = [];
+      if(sentChats.length > 0){
+  
+        sentChats.map((sentChat: any, i) => {
+          filterredReceivedChats = receivedChats.map((receivedChat: any) => {
+            if (
+              receivedChat.senderId.toString() === sentChat.receiverId.toString()
+            ) {
+              sentChats[i] = undefined;
+            }
+            return receivedChat;
+          });
         });
-      });
+      }
+      const chats = [sentChats, filterredReceivedChats];
+      res.status(201).json(chats.flat());
+    } catch (err) {
+      console.log(err);
+      res
+        .status(400)
+        .json({ message: "Sorry an error occurred,please try again" });
     }
-    const chats = [sentChats, filterredReceivedChats];
-    res.status(201).json(chats.flat());
-  } catch (err) {
-    console.log(err);
+  }else{
     res
-      .status(400)
-      .json({ message: "Sorry an error occurred,please try again" });
+    .status(400)
+    .json({ message: "Invalid request" });
   }
 }
