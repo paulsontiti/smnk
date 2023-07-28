3a313e59c752f4c47a8c8560c562130e034ec0a1
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-07-28 16:37:02 +0100

    smnk

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    MONGODB_URI : 'mongodb://127.0.0.1:27017/smnk',
+    MONGODB_URI : 'godb+srv://titidprogrammer:30041987Titile@cluster0.69e5w7x.mongodb.net/?retryWrites=true&w=majority',
     //'mongodb+srv://titidprogrammer:30041987Titile@cluster0.69e5w7x.mongodb.net/?retryWrites=true&w=majority',
     //'mongodb://127.0.0.1:27017/smnk',
-    SMNK_URL: 'http://localhost:3000/',
+    SMNK_URL: 'https://www.smnklimited.com/',
     //https://www.smnklimited.com/',
     //''http://localhost:3000/',
     CUSTOMER_SERVICE_ID:'64956c7272a51133486ca7a5'
