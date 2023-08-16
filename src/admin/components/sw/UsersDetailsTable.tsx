466284764568871e226ea97e6b8e8ba143cf3300
paulsontiti@gmail.com <paulsontiti@gmail.com>
2023-08-16 18:40:49 +0100

    smnk

--- src/admin/components/sw/UsersDetailsTable.tsx
+++ src/admin/components/sw/UsersDetailsTable.tsx
@@ -99,7 +99,7 @@ export default function UsersDetailsTable({ users }: { users: any[] }) {
   );
 
   return (
-    <div style={{ height: "auto", width: "100%" }}>
+    <div style={{ height: 400, maxHeight: "auto", width: "100%" }}>
       <DataGrid
         sx={{ margin: "1rem" }}
         getRowId={(row) => row._id}
